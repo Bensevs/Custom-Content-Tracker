805bef4824fc0eee94a993e0d5f0573c30209109
unknown <orhemin@gmail.com>
2025-08-09 22:29:27 +0330

    Fix widget registration and add build assets

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hemin/custom-content-ticker',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5d9e5d697ac3b878a46db94b00f439801f186d21',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hemin/custom-content-ticker' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5d9e5d697ac3b878a46db94b00f439801f186d21',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
